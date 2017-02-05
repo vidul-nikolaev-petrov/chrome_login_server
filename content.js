cd496fd9fd36f72cfd906e82e50d5a233a7498f4
Vidul Nikolaev Petrov <loadinfo.net@gmail.com>
2017-02-05 19:21:12 +0200

    Add the extention

---
+++ content.js
@@ -0,0 +1,10 @@
+// start settings
+var id_username = 'id_username',
+    id_password = 'id_password',
+    username = 'your username',
+    password = 'your password';
+// end settings
+
+document.getElementById(id_username).value = username;
+document.getElementById(id_password).value = password;
+document.forms[0].submit();
