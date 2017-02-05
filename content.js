6d6e03a811c8bba75ae1a0a34bb155fc49372155
Vidul Nikolaev Petrov <loadinfo.net@gmail.com>
2017-02-05 19:26:25 +0200

    Add descriptive comments

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // start settings
-var id_username = 'id_username',
-    id_password = 'id_password',
+var id_username = 'Id of the username element', // for example 'id_username'
+    id_password = 'Id of the password element', // for example 'id_password'
     username = 'your username',
     password = 'your password';
 // end settings
